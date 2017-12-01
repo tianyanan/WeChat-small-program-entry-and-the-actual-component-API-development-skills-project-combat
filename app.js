3780077c47b01cf87f4d955bd1ab448f1e3e062d
tianyanan <57908561@qq.com>
2017-12-01 17:12:57 +0800

    添加id所对应的文章音乐的播放状态

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-    console.log('我在app中的onLaunch方法中');
-    // 登录
-    wx.login({
-      success: res => {
-        // res.code为登录凭证
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
+  globalData:{
+    gIsPlayingMusic:{}
   }
-})
+});
 
 
 //总结：
   // 1：只有在app.json中注册过的页面， 小程序才认为是页面;
   // 2：每次请求页面时， Page对象中所有data数据都会初始化为最初 " 状态/数据 ";
   // 3：事件处理函数也叫作事件监听器
+  // 4：app.js中的app对象为全局对象， 所有的页面都可以使用app对象中的数据; 在其他页面中使用app对象数据（变量）需要显示的调用getApp()方法来获取到全局对象（app对象）
